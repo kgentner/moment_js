d4885e30f587737e6ccd41e6c0d2342c8182ef3e
kgentner <gentnerkarl@gmail.com>
2014-11-02 22:24:00 -0800

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      all: ['server.js'],
+      options: {
+        jshintrc: true
+      }
+    },
+    jscs: {
+      all: {
+        options: {
+          //standard: 'airbnb'
+          // 'preset':'aribnb'
+        },
+        files: {
+          src: [ 'server.js']
+        }
+      }
+    },
+
+  });
+
+  grunt.registerTask('test', ['jshint','jscs']);
+  grunt.registerTask('default',  ['test']);
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+var express = require('express');
+var moment = require('moment');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser());
+var port = 3000;
+//
+//reset moment.js's humanize thresholds
+moment.relativeTimeThreshold('d', 365);
+moment.relativeTimeThreshold('M', 60);
+
+app.get('/', function(req, res){
+  var html = '<h3>Enter a date to see the difference in time from today.</h3>' +
+    '<br><form action="/" method="post">' +
+    ' Enter Date: <input type="text" name="inputDate" ' +
+    'placeholder="'+
+    moment().format('MM-DD-YYYY')+
+    '"/><button type="submit" value="Submit">' +
+    'Submit</button></form>';
+
+  res.send(html);
+});
+
+app.post('/', function(req, res){
+  var now = moment();
+  var dateEntered = moment(req.body.inputDate);
+  var dateDif = dateEntered.diff(now);
+  var humanDif = moment.duration(dateDif).humanize(true);
+  var html='<h2>'+dateEntered.format('MM-DD-YYYY')+' occurs '+humanDif+' </h2>';
+
+  res.send(html);
+});
+
+app.listen(port);
+
+console.log('Server running on port: ' + port);
